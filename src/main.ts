a9009871a2eeefda249cc1c792bfb022af9f988f
unknown <serjant.saygon@gmail.com>
2023-01-29 15:17:47 +0600

    added rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-import {IInfo} from "./interfaces/IInfo";
+import client, {
+    Connection,
+    Channel,
+    ConsumeMessage
+} from "amqplib";
 
-const myInfo: IInfo = {
-    code: 9,
-    email: "serjant.saygon@gmail.com",
-    name: "Sergey",
-    phone: "+79112323123",
+//function to send some messages before consuming the queue
+const sendMessages = (channel: Channel) => {
+    for (let i = 0; i < 10; i++) {
+       channel.sendToQueue('myQueue', Buffer.from(`Message ${i+1}`))
+    }
 }
 
-const main = (info: IInfo): string => {
-    return `Hello, I'm ${info.name}, my code is ${info.code},
-e-mail ${info.email} and phone ${info.phone}`
+//consumer for the queue
+// We use currying to give it the channel required  to acknowledge the message
+const consumer = (channel: Channel) => (msg: ConsumeMessage | null): void => {
+    if (msg) {
+        //Display the received message
+        console.log(msg.content.toString())
+        // Acknowledge the message
+        channel.ack(msg)
+    }
 }
 
-console.log(main(myInfo))
+const start = async () => {
+    const connection: Connection = await client.connect('amqp://username:password@localhost:5672')
+    //Create a channel
+    const channel: Channel = await connection.createChannel()
+    // Makes the queue available to the client
+    await channel.assertQueue('myQueue')
+    // Send some messages to the queue
+    sendMessages(channel)
+    // Start the consumer
+    await channel.consume('myQueue', consumer(channel))
+}
+
+start().then()
